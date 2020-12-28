3bd6b25e29e18d94f6793147b244c46ded5dc606
3l-d1abl0 <sameer.barha12@gmail.com>
2020-12-28 14:56:07 +0530

    added new endpoint to place Bid

---
+++ service-en-auction/src/handlers/placeBid.js
@@ -0,0 +1,47 @@
+import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import htpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+
+    const params = {
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Key: { id },
+      UpdateExpression: 'set highestBid.amount = :amount',
+      ExpressionAttributeValues: {
+        ':amount': amount,
+      },
+      ReturnValues: 'ALL_NEW',
+    };
+
+let updatedAuction;
+  try{
+
+    const result = await dynamodb.update(params).promise();
+    updatedAuction = result.Attributes;
+
+  }catch(error){
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+export const handler = middy(placeBid)
+.use(httpJsonBodyParser())
+.use(httpEventNormalizer())
+.use(htpErrorHandler());
+
+
