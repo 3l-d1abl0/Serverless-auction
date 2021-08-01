6845b7b88b3b4576aa5dda9d7deacc0a8b3d861a
sameer <sameer.barha12@gmail.com>
2021-08-01 12:26:42 +0530

    added middlewares and added request schema with middy validator

--- service-en-auction/src/handlers/createAuction.js
+++ service-en-auction/src/handlers/createAuction.js
@@ -1,8 +1,9 @@
 import AWS from 'aws-sdk';
 import { v4 as uuid } from 'uuid';
 import createError from 'http-errors';
+import validator from '@middy/validator';
 import commonMiddleware from '../lib/commonMiddleware';
-
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -43,4 +44,5 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+  .use(validator({ inputSchema: createAuctionSchema}));
