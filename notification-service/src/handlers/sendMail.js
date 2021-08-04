a4f60f0206a7ccc0aa840c1e8f3a8e11c0b087c8
sameer <sameer.barha12@gmail.com>
2021-08-04 21:04:28 +0530

    added SQS message processing !

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -5,19 +5,25 @@ const ses = new AWS.SES({ region: 'ap-south-1'});
 
 async function sendMail(event, context ){
 
+    const record = event.Records[0];
+    console.log('Email Processing ', record);
+
+    const email = JSON.parse(record.body);
+    const { subject, body, recipient } = email;
+
     const params ={
-        Source: 'verified@email.com',
+        Source: 'sameer.generalist@gmail.com',
         Destination: {
-            ToAddresses: ['email@example.com'],
+            ToAddresses: [recipient],
         },
         Message: {
             Body: {
                 Text: {
-                    Data: 'Bonsoir Elliot !'
+                    Data: body,
                 },
             },
             Subject: {
-                Data: 'Test Mail',
+                Data: subject,
             },
         },
     };
@@ -28,6 +34,6 @@ async function sendMail(event, context ){
         return result;
     }catch(error){
         console.error(error);
-    } 
+    }
 }
 export const handler = sendMail;
