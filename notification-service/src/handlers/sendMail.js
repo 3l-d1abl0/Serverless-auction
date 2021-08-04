2e35e4ef1bea61d79102169849ffa7cc3b6cf2e5
sameer <sameer.barha12@gmail.com>
2021-08-04 19:23:45 +0530

    added emails via SES

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,9 +1,33 @@
+import AWS from 'aws-sdk';
+
+
+const ses = new AWS.SES({ region: 'ap-south-1'});
+
 async function sendMail(event, context ){
 
-    console.log(event);
+    const params ={
+        Source: 'verified@email.com',
+        Destination: {
+            ToAddresses: ['email@example.com'],
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: 'Bonsoir Elliot !'
+                },
+            },
+            Subject: {
+                Data: 'Test Mail',
+            },
+        },
+    };
 
-    return event;
-    
+    try{
+        const result = await ses.sendEmail(params).promise();
+        console.log(result);
+        return result;
+    }catch(error){
+        console.error(error);
+    } 
 }
-
 export const handler = sendMail;
