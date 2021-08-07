4234ef7f120b2ec604f7e79c18b62f9ad555d24b
sameer <sameer.barha12@gmail.com>
2021-08-07 19:46:10 +0530

    added image upload functionality

--- service-en-auction/src/handlers/uploadAuctionPicture.js
+++ service-en-auction/src/handlers/uploadAuctionPicture.js
@@ -1,4 +1,24 @@
+import { getAuctionById } from "./getAuction";
+import { uploadPicturetoS3 } from "../lib/uploadPictureToS3";
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import createHttpError from "http-errors";
+
 export async function uploadAuctionPicture(event){
+
+    const { id } = event.pathParameters;
+    const auction = await getAuctionById(id);
+    const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+    const buffer = Buffer.from(base64, 'base64');
+
+    try{
+    const uploadToS3Result = await uploadPicturetoS3(auction.id+'.jpg', buffer);
+    console.log(uploadToS3Result);
+    }catch(error){
+        console.error(error);
+        throw new createHttpError.InternalServerError(error);
+    }
+
 
     return{
         statusCode: 200,
@@ -7,4 +27,5 @@ export async function uploadAuctionPicture(event){
 
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture)
+    ,use(httpErrorHandler());
